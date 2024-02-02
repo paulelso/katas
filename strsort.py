f646c2e3c6334910dc1cdf037d8d5fb7dd2c280a
psousa@twilio.com <paulelso@hotmail.com>
2024-02-02 22:25:51 +0100

    Updated version of strsort.py

--- strsort.py
+++ strsort.py
@@ -1,16 +1,17 @@
+# Description: Function takes a string and returns a string  
+# with the letters in each word sorted alphabetically.
+# String with spaces is split into a list of words. The words are
+# sorted alphabetically and joined into a string with a comma and space.
 def strsort(s):
     words = s.split(' ')
-    if words != None:
-        sorted_str = []
-        for word in words:
-             sorted_str.append(''.join(sorted(word)))
+    sorted_lst = []
+    for word in words:
+        sorted_lst.append(''.join(sorted(word)))
+    return ', '.join(sorted_lst)    
     
-    print(sorted_str)
 
 if __name__ == '__main__':
-    """
     print(strsort('cba'))
     print(strsort('bdac'))
     print(strsort('Tom Dick Harry'))
-    """
-    strsort('Tom Dick Harry')
+    print(strsort('Tom Dick Harry'))
