f25d2f38f8742783c00ea0409d0ce361c1b7c3b8
psousa@twilio.com <paulelso@hotmail.com>
2024-02-02 22:38:44 +0100

    Committed last_word.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-s = "apple orange banana"
-words = s.split()
-print(words[-1][-1])
+with open("./file.txt", "w") as file:
+    file.write("apple banana cherry date elderberry fig grapefruit honeydew kiwi lemon mango nectarine orange pear quince raspberry strawberry zucchini tangerine ugli fruit valencia orange watermelon xigua yellow passion fruit\n")
+
+with open("./file.txt", "r") as file:
+    words = file.read().split()
+    last_word = max(words, key=str.lower)
+    print("The last word alphabetically is:", last_word)
