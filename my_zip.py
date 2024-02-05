f06f2c9ab0054d7db63b39fc366d6cf3d3c3dd4e
psousa@twilio.com <paulelso@hotmail.com>
2024-02-05 23:34:36 +0100

    Modified my_zip.py to return a list

--- my_zip.py
+++ my_zip.py
@@ -1,8 +1,12 @@
 # Description: This is a simple implementation of the zip function in Python.
 def my_zip(list1, list2):
+    zip_list = []
     for i in range(len(list1)):
-        print(f"{list1[i]}, {list2[i]}")
+        zip_list.append((list1[i], list2[i]))
+        
+    return zip_list
         
 
 if __name__ == "__main__":
-   my_zip([1, 2, 3], "abc")
+   print(my_zip([1, 2, 3], "abc"))# [(1, 'a'), (2, 'b'), (3, 'c')]
+   print(my_zip([10, 20, 30], "abc"))# [(10, 'a'), (20, 'b'), (30, 'c')]
