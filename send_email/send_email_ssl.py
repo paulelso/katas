d57b820341324c43a53acf45bd4dd791f0098714
psousa@twilio.com <paulelso@hotmail.com>
2024-02-08 13:10:13 +0100

    Added send email option.

--- send_email/send_email_ssl.py
+++ send_email/send_email_ssl.py
@@ -3,10 +3,18 @@ import smtplib, ssl
 smtp_server = "smtp.gmail.com"
 port = 465  # For SSL
 
-sender_email = "psousa@twilio.com"  # Enter your address
+sender_email = 'psousa@twilio.com'  # Enter your address
+sender_name = 'Paulo Sousa (Twilio)'
 password = input("Type your password and press enter: ")
 
-import ssl
+receiver_email = 'paulelso@gmail.com'
+message = """\
+From: {}
+To: {}
+Subject: Hi there!
+
+This message is sent from Python!
+""".format(sender_email, receiver_email)
 
 # Create an unverified context
 context = ssl._create_unverified_context()
@@ -18,4 +26,4 @@ context = ssl._create_unverified_context()
 # Initiate connection, do something with the connection, close the connection.
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password) # Log in to the server
-    print("Connected to the server")
+    server.sendmail(sender_email, receiver_email, message) # Send the email
