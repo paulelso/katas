710d598de6411291d3a9b2ef0e0d01aa1f092368
psousa@twilio.com <paulelso@hotmail.com>
2024-02-14 22:51:28 +0100

    rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -14,22 +14,21 @@ def get_computer_selection():
     return computer_selection
 
 
-def get_winner(user_selection, computer_selection):
-    if user_selection == computer_selection:
-        return "It's a tie"
-    elif user_selection == 'r' and computer_selection == 's':
-        return "Rock beats Scissors. You win!"
-    elif user_selection == 'p' and computer_selection == 'r':
-        return "Paper beats Rock. You win!"
-    elif user_selection == 's' and computer_selection == 'p':
-        return "Scissors beats Paper. You win!"
-    elif computer_selection == 'r' and user_selection == 's':
-        return "Rock beats Scissors. You lose!"
-    elif computer_selection == 'p' and user_selection == 'r':
-        return "Paper beats Rock. You lose!"
-    elif computer_selection == 's' and user_selection == 'p':
-        return "Scissors beats Paper. You lose!"
-    
+def get_winner(p1, p2):
+    options = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
+    p1 = options[p1]
+    p2 = options[p2]
+    combinations = [('r', 's'), ('s', 'p'), ('p', 'r')]
+    scores = f"You: {p1} vs Computer: {p2}."
+    if (p1, p2)  in combinations:
+        winner = f"{scores} You win!"
+    elif (p2, p1) in combinations:
+        winner = f"{scores} Computer wins!"
+    else:
+        winner = f"{scores} It's a tie"
+
+    return winner
+
 
 if __name__ == "__main__":
     user_selection = get_user_selection()
