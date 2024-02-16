708c2664ddd23e0f847797b5ea91730339a27c69
psousa@twilio.com <paulelso@hotmail.com>
2024-02-16 22:29:52 +0100

    mad_libs_generator.py

---
+++ mad_libs_generator.py
@@ -0,0 +1,15 @@
+noun = input("Enter a noun: ") #ask the user to enter a noun
+p_noun = input("Enter a plural noun: ") #ask the user to enter a plural noun
+noun2 = input("Enter a noun: ") #ask the user to enter a noun
+place = input("Enter a name of a place: ") #ask the user to enter a name of a place
+adjective = input("Enter an adjective: ") #ask the user to enter an adjective
+noun3 = input("Enter a noun: ") #ask the user to enter a noun
+
+print(f'------------------------------------------\n'
+    f'Be kind to your {noun}-footed {p_noun}\n'
+    f'For a duck may be somebody\'s {noun2},\n'
+    f'Be kind to your {p_noun} in {place}\n'
+    f'Where the weather is always {adjective}.\n'
+    f'You may think that is this the {noun3},\n'
+    f'Well it is.\n'
+    f'------------------------------------------')
